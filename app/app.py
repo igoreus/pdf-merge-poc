0af12b555a41e0cf879de6eda04ba11c4cee97af
Igor Veremchuk <igorveremchuk@sellercenter-bunker.local>
2017-08-04 10:34:38 +0200

    add logging

--- app/app.py
+++ app/app.py
@@ -2,18 +2,23 @@ from flask import Flask, request, jsonify, make_response
 import pdf
 import base64
 import uuid
-import json
+import logging
+import tempfile
 
 app = Flask(__name__)
+logging.basicConfig(filename=tempfile.gettempdir() + '/app.log', level=logging.ERROR)
+
 
 @app.route("/health-check")
 def health_check():
     return "OK"
 
+
 @app.route("/pdf-merger", methods=['POST'])
 def merge():
     res = pdf.merge(request.get_json(force=True))
     return res
+
 
 @app.route("/pdf-merger-stream", methods=['POST'])
 def merge_stream():
@@ -23,6 +28,18 @@ def merge_stream():
     response.headers['Content-Disposition'] = \
         'inline; filename=%s.pdf' % str(uuid.uuid4())
     return response
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    logging.error('Server Error: %s', error)
+    return 'Error 500', 500
+
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    logging.error('Unhandled Exception: %s', e)
+    return 'Error 500', 500
 
 if __name__ == "__main__":
     app.run()
--- app/pdf.py
+++ app/pdf.py
@@ -3,23 +3,28 @@ from PyPDF2 import PdfFileMerger, PdfFileReader
 import base64
 import tempfile
 import StringIO
+import magic
 
 
-def merge(pdf_base64_encoded_list = None):
+def merge(pdf_base64_encoded_list=None):
+    pdf_temp_file_list = []
 
-    pdf_temporaty_file_list = []
     for i in pdf_base64_encoded_list:
+        binary_string = base64.urlsafe_b64decode(str(i))
+
+        if magic.from_buffer(binary_string, mime=True) != 'application/pdf':
+            raise Exception('Wrong file. Mime type should be application/pdf')
+
         f = tempfile.SpooledTemporaryFile()
-        f.write(base64.urlsafe_b64decode(str(i)))
-        pdf_temporaty_file_list.append(f)
+        f.write(binary_string)
+        pdf_temp_file_list.append(f)
 
     merger = PdfFileMerger(strict=False)
 
-    for pdf_file in pdf_temporaty_file_list:
+    for pdf_file in pdf_temp_file_list:
         merger.append(PdfFileReader(pdf_file, strict=False))
 
     pdf_merged_buffer = StringIO.StringIO()
     merger.write(pdf_merged_buffer)
 
     return base64.urlsafe_b64encode(pdf_merged_buffer.getvalue())
-
