ff244220df6ed10be5456ef97f2a2cc04d6e5497
Dennis Munchausen <dennis.munchausen@rocket-internet.de>
2018-03-29 11:02:46 +0200

    Change of magic version to python-magic 0.4.15 and using during pdf merging BytesIO instead of StringIO

--- app/pdf.py
+++ app/pdf.py
@@ -2,7 +2,7 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import base64
 import tempfile
-from io import StringIO
+from io import BytesIO
 import magic
 
 
@@ -11,8 +11,9 @@ def merge(pdf_base64_encoded_list=None):
 
     for i in pdf_base64_encoded_list:
         binary_string = base64.urlsafe_b64decode(str(i))
+        mime_type = magic.from_buffer(binary_string, mime=True)
 
-        if magic.from_buffer(binary_string, mime=True) != 'application/pdf':
+        if mime_type != 'application/pdf':
             raise Exception('Wrong file. Mime type should be application/pdf')
 
         f = tempfile.SpooledTemporaryFile()
@@ -24,7 +25,7 @@ def merge(pdf_base64_encoded_list=None):
     for pdf_file in pdf_temp_file_list:
         merger.append(PdfFileReader(pdf_file, strict=False))
 
-    pdf_merged_buffer = StringIO.StringIO()
+    pdf_merged_buffer = BytesIO()
     merger.write(pdf_merged_buffer)
 
     return base64.urlsafe_b64encode(pdf_merged_buffer.getvalue())
